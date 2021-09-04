bcca48db6061b49b10b381042734e1468a282334
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-09-04 14:14:09 +0300

    Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personalCar = new PersonalCar
+            {
+                Brand = "BMW",
+                HirePrice = 1000,
+                Model = "3"
+            };
+            var specialOffer = new SpecialOffer(personalCar);
+            Console.WriteLine(specialOffer.HirePrice);
+        }
+        public abstract class CarBase
+        {
+            public abstract string Brand { get; set; }
+            public abstract string Model { get; set; }
+            public abstract decimal HirePrice { get; set; }
+        }
+        public class CommercialCar : CarBase
+        {
+            public override string Brand { get; set; }
+            public override string Model { get; set; }
+            public override decimal HirePrice { get; set; }
+        }
+        public class PersonalCar : CarBase
+        {
+            public override string Brand { get; set; }
+            public override decimal HirePrice { get; set; }
+            public override string Model { get; set; }
+
+        }
+        public abstract class CarDecoratorBase : CarBase
+        {
+            CarBase _carBase;
+            public CarDecoratorBase(CarBase carBase)
+            {
+                _carBase = carBase;
+            }
+        }
+        public class SpecialOffer : CarDecoratorBase
+        {
+            private readonly CarBase carBase;
+            public SpecialOffer(CarBase carBase) : base(carBase)
+            {
+                this.carBase = carBase;
+            }
+            public override string Brand { get; set; }
+            public override decimal HirePrice { get { return carBase.HirePrice * 90 / 100; } set { } }
+            public override string Model { get; set; }
+        }
+    }
+}
---
+++ DecoratorRecap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace DecoratorRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transitOrder = new TransitOrder();
+            transitOrder.Price = 500;
+
+            SpescialCustomerOffer spescialCustomerOffer = new SpescialCustomerOffer(transitOrder);
+            Console.WriteLine(spescialCustomerOffer.Price);
+        }
+    }
+    public abstract class OrderBase
+    {
+        public abstract decimal Price { get; set; }
+        public abstract string From { get; set; }
+    }
+    public class NormalOrder : OrderBase
+    {
+        public override string From { get; set; }
+        public override decimal Price { get; set; }
+    }
+    public class TransitOrder : OrderBase
+    {
+        public override string From { get; set; }
+        public override decimal Price { get; set; }
+    }
+    public abstract class OrderBaseDecorator : OrderBase
+    {
+        OrderBase orderBase;
+        public OrderBaseDecorator(OrderBase orderBase)
+        {
+            this.orderBase = orderBase;
+        }
+    }
+    public class SpescialCustomerOffer:OrderBaseDecorator
+    {
+        OrderBase orderbase;
+        public SpescialCustomerOffer(OrderBase orderBase):base(orderBase)
+        {
+            this.orderbase = orderBase;
+        }
+        public override string From { get; set ; }
+        public override decimal Price { get { return orderbase.Price * 95 / 100; } set { } }
+    }
+}

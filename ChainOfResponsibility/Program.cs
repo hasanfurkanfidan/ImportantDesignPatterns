ab59e8d34404324a30c53648e57e9bfe2356a50b
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-09-04 16:45:18 +0300

    Chain Of Responsibility

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            VicePresident vicePresident = new VicePresident();
+            President president = new President();
+
+            manager.SetSuccessor(vicePresident);
+            vicePresident.SetSuccessor(president);
+
+            Expense expense = new Expense();
+            expense.Detail = "Detay";
+            expense.Amount = 1500;
+
+            manager.HandleExpense(expense);
+        }
+    }
+    public class Expense
+    {
+        public string Detail { get; set; }
+        public decimal Amount { get; set; }
+    }
+    public abstract class ExpenseHandlerBase
+    {
+        protected ExpenseHandlerBase _successor;
+        public abstract void HandleExpense(Expense expense);
+        public void SetSuccessor(ExpenseHandlerBase successor)
+        {
+            _successor = successor;
+        }
+    }
+    public class Manager : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount <= 100)
+            {
+                Console.WriteLine("Manager handled the expence");
+            }
+            else if (_successor != null)
+            {
+                _successor.HandleExpense(expense);
+            }
+        }
+    }
+    public class VicePresident : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 100 && expense.Amount <= 1000)
+            {
+                Console.WriteLine("Vice President handled the expence");
+            }
+            else if (_successor != null)
+            {
+                _successor.HandleExpense(expense);
+            }
+        }
+    }
+    public class President : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 1000)
+            {
+                Console.WriteLine("President handled the expence");
+            }
+            else if (_successor != null)
+            {
+                _successor.HandleExpense(expense);
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibilityRecap/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ChainOfResponsibilityRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+            employee.Status = "Mid";
+
+            MidEngineer midEngineer = new MidEngineer();
+            SeniorEngineer seniorEngineer = new SeniorEngineer();
+
+            midEngineer.SetSuccessor(seniorEngineer);
+
+            midEngineer.Handle(employee);
+        }
+        public class Employee
+        {
+            public string Status { get; set; }
+        }
+        public abstract class EmployeeInterviewHandler
+        {
+            protected EmployeeInterviewHandler Successor;
+            public abstract void Handle(Employee employee);
+
+            public void SetSuccessor(EmployeeInterviewHandler employeeInterviewHandler)
+            {
+                Successor = employeeInterviewHandler;
+            }
+        }
+        public class MidEngineer : EmployeeInterviewHandler
+        {
+            public override void Handle(Employee employee)
+            {
+                if (employee.Status=="Juniour")
+                {
+                    Console.WriteLine("Employee handled");
+                }
+                if (Successor!=null)
+                {
+                    Successor.Handle(employee);
+                }
+            }
+        }
+        public class SeniorEngineer:EmployeeInterviewHandler
+        {
+            public override void Handle(Employee employee)
+            {
+                if (employee.Status == "Mid")
+                {
+                    Console.WriteLine("Employee handled");
+                }
+               
+            }
+        }
+    }
+}

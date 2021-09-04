e374d45e7ae9e629d347e3d70df541db4a289cb5
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-09-04 18:35:26 +0300

    İlerleme sağlandı

---
+++ NullObject/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class CustomerManager
+    {
+        private readonly ILogger _logger;
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            _logger.Log();
+        }
+    }
+    class NullObjectLogger : ILogger
+    {
+        static object _lock = new object();
+        static NullObjectLogger _nullObjectLogger;
+        private NullObjectLogger()
+        {
+
+        }
+        public static NullObjectLogger CreateAsSinglton()
+        {
+            if (_nullObjectLogger == null)
+            {
+                lock (_lock)
+                {
+                    _nullObjectLogger = new NullObjectLogger();
+                }
+            }
+            return _nullObjectLogger;
+        }
+        public void Log()
+        {
+
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class NLogLogger : ILogger
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CustomerManagerTest
+    {
+        CustomerManager customerManager = new CustomerManager(NullObjectLogger.CreateAsSinglton());
+    }
+}

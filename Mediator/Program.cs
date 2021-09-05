791b8832d1d05f74d6768b274b53846c5f38fd96
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-09-05 17:44:45 +0300

    Mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            Teacher teacher = new Teacher(mediator);
+            var student = new Student(mediator);
+            student.Name = "Furkan";
+            mediator.Students = new List<Student>();
+            mediator.Students.Add(student);
+            teacher.SendNewImageUrl("url");
+
+        }
+    }
+    public abstract class CourseMember
+    {
+        protected readonly Mediator Mediator;
+        public CourseMember(Mediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+    public class Teacher : CourseMember
+    {
+        public Teacher(Mediator mediator) : base(mediator)
+        {
+
+        }
+        public void ReceiveQuestion(string questionText, Student student)
+        {
+            Console.WriteLine($"Teacher received Question from {0},{1}", student.Name, questionText);
+        }
+        public void SendNewImageUrl(string url)
+        {
+            Console.WriteLine("The teacher changed the image");
+            Mediator.UpdateImage(url);
+        }
+        public void AnswerQuestion(string answer,Student student)
+        {
+            Console.WriteLine($"Teacher answered question {student.Name} {answer}");
+        }
+    }
+    public class Student : CourseMember
+    {
+        public Student(Mediator mediator) : base(mediator)
+        {
+
+        }
+        public string Name { get; set; }
+        internal void ReceiveImage(string url)
+        {
+            Console.WriteLine($"Student received image:{url}");
+        }
+
+        internal void ReceiveAnswer(string answerText)
+        {
+            Console.WriteLine($"Student received answer{answerText}");
+        }
+    }
+    public class Mediator
+    {
+        public Teacher Teacher { get; set; }
+        public List<Student> Students { get; set; }
+        public void UpdateImage(string url)
+        {
+            foreach (var student in Students)
+            {
+                student.ReceiveImage(url);
+            }
+        }
+        public void SenQuestion(string questionText, Student student)
+        {
+            Teacher.ReceiveQuestion(questionText, student);
+        }
+       
+        public void SendAnswer(string answerText, Student student)
+        {
+            student.ReceiveAnswer(answerText);
+        }
+    }
+}
---
+++ MediatorRecap/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediatorRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            var teamLead = new TeamLead(mediator);
+            var furkan = new Member(mediator);
+            teamLead.Name = "Ahmet";
+            furkan.Name = "Furkan";
+            var mehmet = new Member(mediator);
+            mehmet.Name = "Mehmet";
+            mediator.TeamLead = teamLead;
+            mediator.Members.Add(furkan);
+            mediator.Members.Add(mehmet);
+            teamLead.AddAnoounce("Merhaba");
+        }
+    }
+    public abstract class Engineer
+    {
+        protected Mediator Mediator;
+        public Engineer(Mediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+    public class TeamLead:Engineer
+    {
+        public TeamLead(Mediator mediator):base(mediator)
+        {
+
+        }
+        public void AddAnoounce(string text)
+        {
+            Console.WriteLine($"Added Announce {text}");
+            Mediator.Announce();
+        }
+        public string Name { get; set; }
+    }
+    public class Member:Engineer
+    {
+        public Member(Mediator mediator):base(mediator)
+        {
+
+        }
+        public void ReceiveAnnouncement()
+        {
+            Console.WriteLine($"{Name} received message");
+        }
+        public string Name { get; set; }
+    }
+    public class Mediator
+    {
+        public TeamLead TeamLead { get; set; }
+        public List<Member> Members { get; set; } = new List<Member>();
+
+        public void Announce()
+        {
+            foreach (var member in Members)
+            {
+                member.ReceiveAnnouncement();
+            }
+        }
+    }
+}
